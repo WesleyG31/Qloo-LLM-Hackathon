7246b32e7b4a2282c968c72a50e44ff2f5d1300b
WesleyG31 <wes.gb31@gmail.com>
2025-07-30 17:32:01 +0200

    cors api

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from src.logger import get_logger
 from src.custom_exception import CustomException
 from core.recommendation_pipeline import generate_recommendations
+
 import os
+
+app = FastAPI()
+logger = get_logger("main")
 
 # Locally -----
 from dotenv import load_dotenv
 load_dotenv()
 # Locally ------
 
-app = FastAPI()
-logger = get_logger("main")
+# Allow your frontend origin (http://localhost:5173)
+#origins = [
+ #   "http://localhost:5173",
+  #  "http://127.0.0.1:5173",
+    # you can add other origins here if needed
+#]
+
+#app.add_middleware(
+ #   CORSMiddleware,
+  #  allow_origins=origins,  # ✅ Use the variable here!
+   # allow_credentials=True,
+    #allow_methods=["*"],
+    #allow_headers=["*"],
+#)
+
+
+
 
 # Model Config
 LLM_MODEL = "deepseek/deepseek-chat-v3-0324:free"
